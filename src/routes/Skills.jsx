64196531ece60d39c02600104300754e623a5818
btriz <00btrz@gmail.com>
2021-11-20 17:40:51 -0300

    CSS - Skills

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 
+const fundamentals = ['Unix & Bash', 'Git & GitHub', 'HTML', 'CSS', 'JavaScript', 'Testes Unitários 🡦 Jest'];
+const front = ['React', 'Metodologias ágeis', 'React Testing Library', 'Redux'];
+const back = ['SQL', 'Normalização e modelagem de Banco de Dados', 'Docker', 'MongoDB'];
+
+
 export default function Skills() {
   return (
     <div className="category skills">
-      Habilidades
+      <div className="stack fundamentals">
+        <div>
+
+        </div>
+        <ul>
+          { fundamentals.map((item) => <li>{item}</li>) }
+        </ul>
+      </div>
+
+      <div className="stack front">
+        <div>
+          
+        </div>
+        <ul>
+          { front.map((item) => <li>{item}</li>) }
+        </ul>
+      </div>
+
+      <div className="stack back">
+        <div>
+          
+        </div>
+        <ul>
+          { back.map((item) => <li>{item}</li>) }
+        </ul>
+      </div>
+
+      <div className="stack bonus">
+        <p>Bônus!!! Também sei tatuar e tocar pandeiro!</p>
+      </div>
     </div>
   )
 }
