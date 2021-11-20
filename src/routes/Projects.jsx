1f815d65cf0b724734504b95085e8f11a5083b89
btriz <00btrz@gmail.com>
2021-11-20 20:22:59 -0300

    CSS + BASIC STRUCTURE - Projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import projects from '../services/projectDATA';
+import btn from '../images/assets/button.png';
 
 export default function Projects() {
   return (
     <div className="category projects">
-      Projetos
+      <div className="carrousel-wrap"> 
+            { projects.map(({ name, imagePath, link }) => (
+              <div className="project">
+                <a
+                  href={ link }
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="project-title">
+                    <p>{ name }</p>
+                    <img
+                      src={ btn }
+                      alt="frame illustrating a browser border" />
+                  </div>
+
+                  <div
+                    className="project-thumbnail"
+                    style={{backgroundImage: `url(${imagePath})`}}
+                  />
+                </a>
+              </div>
+            )) }
+       </div>
     </div>
   )
 }
---
+++ src/services/projectDATA.js
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    name: 'Pixel Art',
+    imagePath: 'https://github.com/Btriz/pixel-art/blob/master/thumbnail.png?raw=true',
+    link: 'https://btriz.github.io/pixel-art/',
+    repo: 'https://github.com/Btriz/pixel-art/',
+    hasFront: true
+  },
+  {
+    name: 'React Timer',
+    imagePath: 'https://github.com/Btriz/react-timer/blob/master/thumbnail.png?raw=true',
+    link: 'https://btriz.github.io/react-timer/',
+    repo: 'https://github.com/Btriz/react-timer/',
+    hasFront: true
+  },
+  {
+    name: 'To Do List',
+    imagePath: 'https://github.com/Btriz/todo-list/blob/master/thumbnail.png?raw=true',
+    link: '',
+    repo: 'https://github.com/Btriz/todo-list',
+    hasFront: true
+  }
+];
+
+export default projects;
