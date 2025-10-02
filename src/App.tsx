08d562298a9c9eed6f315b7655af47d179bb303f
btriz <parabeatrizfagundes@gmail.com>
2025-10-02 11:25:46 -0300

    feat: adicionar lazy loading para páginas e Loader no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   Route,
   Routes,
   useLocation,
 } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
-import { Cursor, Navbar } from './components';
-import { Home, About, Projects, Contact, Welcome } from './pages';
+import { Cursor, Navbar, Loader } from './components'; // Adicione o Loader aqui
 import { NextRouteProvider } from './context/NextRouteContext';
 import { init as initEmailJs } from '@emailjs/browser';
 import { useIsMobile } from './hooks/useIsMobile';
+
+// Lazy load das páginas
+const Welcome = React.lazy(() => import('./pages/Welcome'));
+const Home = React.lazy(() => import('./pages/Home'));
+const About = React.lazy(() => import('./pages/About'));
+const Projects = React.lazy(() => import('./pages/Projects'));
+const Contact = React.lazy(() => import('./pages/Contact'));
 
 const App = () => {
   const location = useLocation();
@@ -28,23 +34,25 @@ const App = () => {
 
         {!isMobile && <Cursor />}
 
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-          >
-            <Routes location={location}>
-              <Route path="/" element={<Welcome />} />
+        <Suspense fallback={<Loader />}>
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={location.pathname}
+            >
+              <Routes location={location}>
+                <Route path="/" element={<Welcome />} />
 
-              <Route path="/home" element={<Home />} />
+                <Route path="/home" element={<Home />} />
 
-              <Route path="/about" element={<About />} />
+                <Route path="/about" element={<About />} />
 
-              <Route path="/projects" element={<Projects />} />
+                <Route path="/projects" element={<Projects />} />
 
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </motion.div>
-        </AnimatePresence>
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </motion.div>
+          </AnimatePresence>
+        </Suspense>
       </NextRouteProvider>
     </main>
   );
