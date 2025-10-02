a15e50fc0b945929a9e4c37968e274dd32a89bb9
btriz <parabeatrizfagundes@gmail.com>
2025-10-02 12:21:58 -0300

    feat: adicionar @types/node como dependência e refatorar configuração do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
+const plugins: PluginOption[] = [react(), tailwindcss()];
+
+if (process.env.VITE_VISUALIZER === 'true') {
+  plugins.push(visualizer({ open: true, gzipSize: true, brotliSize: true }));
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    visualizer({ open: true, gzipSize: true, brotliSize: true }),
-  ],
+  plugins: plugins,
   base: '/',
   assetsInclude: ['**/*.glb'],
   server: {
@@ -23,14 +25,16 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('three')) {
-              return 'three-vendor';
-            }
-            if (id.includes('framer-motion')) {
-              return 'framer-vendor';
-            }
-            return 'vendor';
+          if (!id.includes('node_modules')) {
+            return;
+          }
+
+          if (id.includes('react') || id.includes('react-dom')) {
+            return 'react-vendor';
+          }
+
+          if (id.includes('three') || id.includes('@react-three')) {
+            return 'three-vendor';
           }
         },
       },
