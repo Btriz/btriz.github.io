9cacc356ee4668a0290ce9bca45cf289fb8f0baf
btriz <parabeatrizfagundes@gmail.com>
2025-10-02 20:13:04 -0300

    feat: atualizar imagens

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,7 +2,7 @@ import mercadolivre from './experiences/mercadolivre.png';
 import blip from './experiences/blip.png';
 import puc from './experiences/puc.png';
 import trybe from './experiences/trybe.png';
-import profile from './profile.jpeg';
+import profile from './profile.jpg';
 
 export {
   mercadolivre,
