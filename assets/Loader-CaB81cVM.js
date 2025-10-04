d0459760d3492277b8d04dfdddedb7474ed660d6
Btriz <parabeatrizfagundes@gmail.com>
2025-10-04 21:33:14 +0000

    Deploying to gh-pages from @ Btriz/btriz.github.io@ef8fe22d32789e45340c90c36dda3ff0df3d28f4 🚀

--- assets/Loader-CaB81cVM.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as e}from"./vendor-DqgBrvw6.js";const t=()=>r.jsx(e,{center:!0,children:r.jsx("div",{className:"loader"})});export{t as L};
---
+++ assets/Loader-RKvmnspp.js
@@ -0,0 +1,1 @@
+import{j as r,F as e}from"./vendor-BkasD6Wj.js";const t=()=>r.jsx(e,{center:!0,children:r.jsx("div",{className:"loader"})});export{t as L};
